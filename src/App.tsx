5e8ee21f71226585b5ade7d8ab5b5525771eb6e0
hxyro <hxyro0@gmail.com>
2022-05-26 12:52:36 +0530

    structure tree modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { QueryClient, QueryClientProvider, QueryCache } from 'react-query'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Thread from './components/Thread'
+import Home from './components/Home'
+import ConnectWallet from './components/ConnectWallet'
 import { Provider as WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider, QueryCache } from 'react-query'
 import { providers } from 'ethers'
-import ChatHistory from './components/ChatHistory'
 
 const provider = providers.getDefaultProvider('http://localhost:8545')
 
@@ -25,7 +27,15 @@ function App() {
   return (
     <WagmiProvider autoConnect provider={provider}>
       <QueryClientProvider client={queryClient}>
-        <ChatHistory topic='hello' />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path='/thread/:id'
+              element={<ConnectWallet element={<Thread />} />}
+            />
+            <Route path='/' element={<ConnectWallet element={<Home />} />} />
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </WagmiProvider>
   )
