b488b070c8cacc4b8baef61afe748489b2a3d5fa
hxyro <hxyro0@gmail.com>
2022-05-26 12:53:04 +0530

    misk

--- src/hooks/useManageMessage.ts
+++ src/hooks/useManageMessage.ts
@@ -20,7 +20,7 @@ const useManageMessage = () => {
   const [signer] = useSigner()
   const provider = useProvider()
   const contract = wagmi.useContract({
-    addressOrName: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+    addressOrName: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
     contractInterface: WebpaiContract.abi,
     signerOrProvider: signer.data || provider,
   })
